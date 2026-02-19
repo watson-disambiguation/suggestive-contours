5f1fb00cb9e163f1a93abc156f4e59d54c524a1e
Will Watson <wdfwatson@gmail.com>
2026-02-18 22:48:36 -0400

    Initial Commit

---
+++ post_process.gd
@@ -0,0 +1,86 @@
+@tool
+class_name PostProcess extends CompositorEffect
+
+@export var edge_radius: float = 1
+@export var edge_portion: float = 0.1
+@export var edge_threshold: float = 0.1
+
+var rd : RenderingDevice
+var shader : RID
+var pipeline : RID
+
+func _init() -> void:
+	# run on rendering thread so main thread isn blocked
+	RenderingServer.call_on_render_thread(initialize_compute_shader)
+	needs_normal_roughness = true;
+
+func _notification(what: int) -> void:
+	# want to free resources before they are deleted
+	# ppeline is freed when shader is freed
+	if what == NOTIFICATION_PREDELETE and shader.is_valid():
+		RenderingServer.free_rid(shader)
+		
+func _render_callback(effect_callback_type: int, render_data: RenderData) -> void:
+	if not rd: return
+	
+	var scene_buffers : RenderSceneBuffersRD = render_data.get_render_scene_buffers()
+	if not scene_buffers: return
+	
+	var size : Vector2i = scene_buffers.get_internal_size()
+	# if size on either axis is 0, can create any work groups
+	if size.x == 0 or size.y == 0: return
+	
+	var x_groups : int = size.x;
+	var y_groups : int = size.y;
+	
+	var push_constants : PackedFloat32Array = PackedFloat32Array()
+	push_constants.append(size.x)
+	push_constants.append(size.y)
+	push_constants.append(edge_radius)
+	push_constants.append(edge_portion)
+	push_constants.append(edge_threshold)
+	# need to pack to fill sufficient bytes
+	push_constants.append(0.0)
+	push_constants.append(0.0)
+	push_constants.append(0.0)
+	
+	# create multiple views in case we are doing stereo rendering for VR
+	for view in scene_buffers.get_view_count(): 
+		var screen_tex : RID = scene_buffers.get_color_layer(view)
+
+		# create uniform for passing screen texture data
+		var uniform_screen : RDUniform = RDUniform.new()
+		uniform_screen.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
+		uniform_screen.binding = 0
+		uniform_screen.add_id(screen_tex)
+		
+		needs_normal_roughness = true;
+		var normal_tex : RID = scene_buffers.get_texture("forward_clustered", "normal_roughness")
+		if not normal_tex: return
+		
+		# create uniform for passing screen normal texture data
+		var uniform_normal : RDUniform = RDUniform.new()
+		uniform_normal.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
+		uniform_normal.binding = 1
+		uniform_normal.add_id(normal_tex)
+		
+		var image_uniform_set : RID = UniformSetCacheRD.get_cache(shader, 0, [uniform_screen, uniform_normal])
+		
+		var compute_list : int = rd.compute_list_begin()
+		rd.compute_list_bind_compute_pipeline(compute_list, pipeline)
+		rd.compute_list_bind_uniform_set(compute_list, image_uniform_set, 0)
+		rd.compute_list_set_push_constant(compute_list,push_constants.to_byte_array(),push_constants.size() * 4)
+		rd.compute_list_dispatch(compute_list, x_groups, y_groups, 1)
+		rd.compute_list_end()
+
+func initialize_compute_shader() -> void:
+	rd = RenderingServer.get_rendering_device()
+	if not rd: return
+	
+	# load in file from data
+	var glsl_file : RDShaderFile = load("res://test.glsl")
+	shader = rd.shader_create_from_spirv(glsl_file.get_spirv())
+	pipeline = rd.compute_pipeline_create(shader)
+	
+
+	
---
+++ test.glsl
@@ -0,0 +1,107 @@
+#[compute]
+#version 450
+
+layout(local_size_x = 1, local_size_y = 1, local_size_z =1) in;
+
+layout(rgba16f, binding = 0, set = 0) uniform image2D screen_tex;
+layout(rgba16f, binding = 1, set = 0) uniform image2D normal_tex;
+
+
+layout(push_constant, std430) uniform Params {
+    vec2 screen_size;
+    float radius;
+    float s;
+    float d;
+} p;
+
+vec3 sample_normals(ivec2 pixel) {
+    vec3 normal_raw = imageLoad(normal_tex, pixel).rgb;
+    return normalize(normal_raw.rgb * 2.0 - 1.0);
+}
+
+float get_diffuse_lit_from_camera(ivec2 pixel) {
+    vec3 normal = sample_normals(pixel);
+    if (normal.z < -0.5) {
+        return 1.0;
+    }
+    return normal.z;
+}
+// returns 1 if valley, 2 if peak, 0 if neither and -1 if background;
+int is_valley_or_peak(ivec2 pixel, int radius, float s, float d) {
+    float p_max = -1.0;
+    float p_min = 2.0;
+    int count = 0;
+    int count_darker = 0;
+    int count_lighter = 0;
+    float center_val = sample_normals(pixel).z;
+    if (center_val < -0.5) {
+        return -1;
+    }
+    for (int x_offset = -radius; x_offset <= radius; x_offset++) {
+        for (int y_offset = -radius; y_offset <= radius; y_offset++) {
+            // check for circular space
+            if (x_offset * x_offset + y_offset * y_offset > radius * radius) {
+                continue;
+            }
+            count++;
+            float val = get_diffuse_lit_from_camera(pixel + ivec2(x_offset,y_offset));
+            p_max = max(p_max,val);
+            p_min = min(p_min,val);
+            if (val < center_val) {
+                count_darker++;
+            }
+            if (val > center_val) {
+                count_lighter++;
+            }
+        }
+    }
+
+    float portion_darker =  float(count_darker) / float(count);
+    float portion_lighter =  float(count_lighter) / float(count);
+    float diff_from_max = p_max - center_val;
+    float diff_from_min = center_val - p_min;
+    if (portion_darker < s && diff_from_max > d) {
+        return 1;
+    }
+    if (portion_lighter < s && diff_from_min > d) {
+        return 2;
+    }
+    return 0;
+}
+
+void main() {
+
+    ivec2 pixel = ivec2(gl_GlobalInvocationID.xy);
+    vec2 size = p.screen_size;
+
+    if (pixel.x >= size.x || pixel.y >= size.y) return;
+
+    vec4 original_color = imageLoad(screen_tex,pixel);
+    vec4 color = vec4(1.0);
+    // starting using greyscale, just use G for now
+    float shading = original_color.g;
+    float threshold = 0.2;
+
+    vec3 line_color_valley = vec3(0.0);
+    vec3 line_color_peak = vec3(1.0);
+    vec3 fill_color = vec3(0.5);
+    vec3 background_color = vec3(1.0);
+
+    vec3 dark_color = vec3(0.0);
+    vec3 light_color = vec3(1.0);
+    
+    int contour_status = is_valley_or_peak(pixel,int(p.radius),p.s,p.d);
+    switch (contour_status) {
+        case 0:
+        case 2:
+            color.rgb = fill_color;
+            break;
+        case 1: 
+            color.rgb = line_color_valley;
+            break;
+        default:
+            color.rgb = background_color;
+    }
+
+    imageStore(screen_tex,pixel,color);
+}
